60ba9f604341fd12803243d4ef1cfbf4a920c81d
chocominticeeeeeeeee <avaparasa58@gmail.com>
2023-09-10 03:00:20 +0900

    9/10-3:00

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export const Form = () => {
+export const Form = (props) => {
     const [name, setName] = useState("");
     const handleNameChange = (event) => {
         setName(event.target.value);
@@ -24,37 +24,30 @@ export const Form = () => {
     };
 
     const handleSubmit = () => {
-        setName("");
-        setMailAddress("");
-        setTitle("");
-        setMessage("");
+        // setTimeout(() => {
+        //     setName("");
+        //     setMailAddress("");
+        //     setTitle("");
+        //     setMessage("");
+        // }, 1000);
         openModal();
-    };
-
-    const formKeys = {
-        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScFl-FUmuOgGlSjMy_nVtbJhQjRc20JMhSkYC9uoqho1q0v6A/formResponse",
-        name: "595514514",
-        mail: "1381327282",
-        title: "1513220957",
-        message: "1894209287",
     };
 
     return (
         <>
-            <form action={formKeys.url} id="form" method="POST" target="hidden_iframe" onSubmit={handleSubmit}>
-                <label htmlFor={"entry." + formKeys.name}>お名前（必須）</label>
-                <input type="text" name={"entry." + formKeys.name} id={"entry." + formKeys.name} value={name} onChange={handleNameChange} placeholder="Name" />
+            <form action={props.formKeys.url} id="form" method="POST" target="hidden_iframe" onSubmit={handleSubmit}>
+                <label htmlFor={"entry." + props.formKeys.name}>お名前（必須）</label>
+                <input type="text" name={"entry." + props.formKeys.name} value={name} onChange={handleNameChange} placeholder="Name" />
 
-                <label htmlFor={"entry." + formKeys.mail}>メールアドレス（必須）</label>
-                <input type="mail" name={"entry." + formKeys.mail} id={"entry." + formKeys.mail} value={mailAddress} onChange={handleMailAddressChange} placeholder="Mail" />
+                <label htmlFor={"entry." + props.formKeys.mail}>メールアドレス（必須）</label>
+                <input type="mail" name={"entry." + props.formKeys.mail} value={mailAddress} onChange={handleMailAddressChange} placeholder="Mail" />
 
-                <label htmlFor={"entry." + formKeys.title}>題名</label>
-                <input type="text" name={"entry." + formKeys.title} id={"entry." + formKeys.title} value={title} onChange={handleTitleChange} placeholder="Title" />
+                <label htmlFor={"entry." + props.formKeys.title}>題名</label>
+                <input type="text" name={"entry." + props.formKeys.title} value={title} onChange={handleTitleChange} placeholder="Title" />
 
-                <label htmlFor={"entry." + formKeys.message}>メッセージ本文</label>
+                <label htmlFor={"entry." + props.formKeys.message}>メッセージ本文</label>
                 <textarea
-                    name={"entry." + formKeys.message}
-                    id={"entry." + formKeys.message}
+                    name={"entry." + props.formKeys.message}
                     cols="30"
                     rows="10"
                     value={message}
--- src/pages/SupportP.jsx
+++ src/pages/SupportP.jsx
@@ -6,13 +6,21 @@ import SendedFormModal from "../components/SendedFormModal";
 
 export const SupportP = () => {
 
+    const formKeys = {
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScFl-FUmuOgGlSjMy_nVtbJhQjRc20JMhSkYC9uoqho1q0v6A/formResponse",
+        name: "595514514",
+        mail: "1381327282",
+        title: "1513220957",
+        message: "1894209287",
+    };
+
     return (
         <>
             <Header page={"privacy"} scrolledClass={true}></Header>
             <div className="supportP contents">
                 <div className="inner">
                     <h2>お問い合わせ</h2>
-                    <Form></Form>
+                    <Form formKeys={formKeys}></Form>
                 </div>
             </div>
             <SendedFormModal></SendedFormModal>
