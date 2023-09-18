827d2345d2a43454c3e5560ee15d6ecc9c4460f0
chocominticeeeeeeeee <avaparasa58@gmail.com>
2023-09-18 22:47:49 +0900

    aa

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ export const Form = (props) => {
     const entryTitle = process.env.REACT_APP_FORM_TITLE;
     const entryMessage = process.env.REACT_APP_FORM_MESSAGE;
 
-    console.log(process.env.REACT_APP_ENVTEST);
+    console.log("a");
 
     const [inputName, setinputName] = useState("");
     const [inputEmail, setinputEmail] = useState("");
