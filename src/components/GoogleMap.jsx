de1318ac6e11ba66ec5e2aed17c02d1805d86766
chocominticeeeeeeeee <avaparasa58@gmail.com>
2023-09-23 22:54:32 +0900

    test4

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React from "react";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
 export const Map = () => {
     const container = {
@@ -12,39 +12,40 @@ export const Map = () => {
         lng: 141.33781368899332,
     };
 
-    const [googleMapAPIKey, setGoogleMapAPIKey] = useState(null);
+    const callNetlifyFunctionGetGoogleMapAPIKey = async () => {
+        try {
+            const endpoint = "/.netlify/functions/getGoogleMapAPI";
+            const response = await fetch(endpoint);
 
-    useEffect(() => {
-        const callNetlifyFunctionGetGoogleMapAPIKey = async () => {
-            try {
-                const endpoint = "/.netlify/functions/getGoogleMapAPI";
-                const response = await fetch(endpoint);
-
-                if (response.ok) {
-                    const data = await response.json();
-                    setGoogleMapAPIKey(data.googleMapAPIKey);
-                } else {
-                    console.error("エラー:", response.status);
-                }
-            } catch (error) {
-                console.error("エラー:", error);
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                console.error("エラー:", response.status);
             }
-        };
+        } catch (error) {
+            console.error("エラー:", error);
+        }
+    };
+    callNetlifyFunctionGetGoogleMapAPIKey().then((r)=>{
+        console.log(r.googleMapAPIKey);
+    })
 
-        callNetlifyFunctionGetGoogleMapAPIKey();
-    }, []);
-
-    return (
-        <div>
-            {googleMapAPIKey ? (
-                <LoadScript id="script-loader" googleMapsApiKey={googleMapAPIKey}>
-                    <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
-                        <Marker position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
-                    </GoogleMap>
-                </LoadScript>
-            ) : (
-                <p>Google Maps APIキーを取得中...</p>
-            )}
-        </div>
-    );
+    const apiKey = "AIzaSyBOOFl7VgudDn4OsqQ_u-ClLNRj7xMXDpo";
+    const existingScript = document.querySelector(`script[src^="https://maps.googleapis.com/maps/api/js?key=${apiKey}"]`);
+    if (existingScript) {
+        return (
+            <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
+                <MarkerF position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
+            </GoogleMap>
+        );
+    } else {
+        return (
+            <LoadScript id="script-loader" googleMapsApiKey={apiKey}>
+                <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
+                    <MarkerF position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
+                </GoogleMap>
+            </LoadScript>
+        );
+    }
 };
