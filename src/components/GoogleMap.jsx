77aff3f75aa04e3a20e76baa82ad1b3367876e41
chocominticeeeeeeeee <avaparasa58@gmail.com>
2023-09-23 23:23:54 +0900

    test

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 
 export const Map = () => {
+    const [googleMapAPIKey, setGoogleMapAPIKey] = useState("");
+
     const container = {
         width: "100%",
         height: "400px",
@@ -12,49 +14,46 @@ export const Map = () => {
         lng: 141.33781368899332,
     };
 
-    const [googleMapAPIKey, setGoogleMapAPIKey] = useState(null);
+    const callNetlifyFunctionGetGoogleMapAPIKey = async () => {
+        try {
+            const endpoint = "/.netlify/functions/getGoogleMapAPI";
+            const response = await fetch(endpoint);
+
+            if (response.ok) {
+                const data = await response.json();
+                setGoogleMapAPIKey(data);
+            } else {
+                console.error("エラー:", response.status);
+            }
+        } catch (error) {
+            console.error("エラー:", error);
+        }
+    };
 
     useEffect(() => {
-        const callNetlifyFunctionGetGoogleMapAPIKey = async () => {
-            try {
-                const endpoint = "/.netlify/functions/getGoogleMapAPI";
-                const response = await fetch(endpoint);
-
-                if (response.ok) {
-                    const data = await response.json();
-                    setGoogleMapAPIKey(data.googleMapAPIKey);
-                } else {
-                    console.error("エラー:", response.status);
-                }
-            } catch (error) {
-                console.error("エラー:", error);
-            }
-        };
-
         callNetlifyFunctionGetGoogleMapAPIKey();
+        return () => {};
     }, []);
 
-    if (googleMapAPIKey === null) {
-        return <div>ロード中...</div>;
-    }
-
-    const apiKey = googleMapAPIKey || "AIzaSyBOOFl7VgudDn4OsqQ_u-ClLNRj7xMXDpo";
-
-    const existingScript = document.querySelector(`script[src^="https://maps.googleapis.com/maps/api/js?key=${apiKey}"]`);
-    
-    if (existingScript) {
-        return (
-            <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
-                <MarkerF position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
-            </GoogleMap>
-        );
+    const apiKey = "AIzaSyBOOFl7VgudDn4OsqQ_u-ClLNRj7xMXDpo";
+    if (googleMapAPIKey !== "") {
+        return <p>ロード中...</p>;
     } else {
-        return (
-            <LoadScript id="script-loader" googleMapsApiKey={apiKey}>
+        const existingScript = document.querySelector(`script[src^="https://maps.googleapis.com/maps/api/js?key=${apiKey}"]`);
+        if (existingScript) {
+            return (
                 <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
                     <MarkerF position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
                 </GoogleMap>
-            </LoadScript>
-        );
+            );
+        } else {
+            return (
+                <LoadScript id="script-loader" googleMapsApiKey={apiKey}>
+                    <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
+                        <MarkerF position={position} icon={"https://maps.google.com/mapfiles/kml/paddle/purple-blank.png"} />
+                    </GoogleMap>
+                </LoadScript>
+            );
+        }
     }
 };
